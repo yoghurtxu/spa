615c87340a8e4e24b1754451282812f3c07f56fc
xianweiwu <503158245@qq.com>
2019-04-01 23:31:56 +0800

    add finish demo

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ let addNum = (num) => num.reduce((a, b) => a + b);
 console.info(addNum(num));
 
 import('./page1/index.js');
+
+setTimeout(() => {
+  document.querySelector('#app').outerHTML = `<div>loading finish</div>`;
+}, 1000);
